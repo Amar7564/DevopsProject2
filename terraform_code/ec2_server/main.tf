f7fd10e1e9999f7e07c4f86ea8305285a77b148c
Amar Satish Mandalapu <140232811+Amar7564@users.noreply.github.com>
2025-02-07 20:39:01 +0530

    Update main.tf

--- terraform_code/ec2_server/main.tf
+++ terraform_code/ec2_server/main.tf
@@ -249,15 +249,15 @@ resource "aws_instance" "my-project3teir" {
 
 # STEP3: GET EC2 USER NAME AND PUBLIC IP 
 output "SERVER-SSH-ACCESS" {
-  value = "ubuntu@${aws_instance.my-ec2.public_ip}"
+  value = "ubuntu@${aws_instance.my-project3teir.public_ip}"
 }
 
 # STEP4: GET EC2 PUBLIC IP 
 output "PUBLIC-IP" {
-  value = "${aws_instance.my-ec2.public_ip}"
+  value = "${aws_instance.my-project3teir.public_ip}"
 }
 
 # STEP5: GET EC2 PRIVATE IP 
 output "PRIVATE-IP" {
-  value = "${aws_instance.my-ec2.private_ip}"
+  value = "${aws_instance.my-project3teir.private_ip}"
 }
